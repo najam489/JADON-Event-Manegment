cd9c4f3b06fb82330040448386af500e2e6e1da4
najam489 <47931696+najam489@users.noreply.github.com>
2020-08-11 02:30:30 +0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,44 @@
+AOS.init();
+
+
+
+// var animations = ["animate__bounceOutLeft", 
+//                     "animate__slideOutLeft", 
+//                     "animate__bounceOutLeft",
+//                     "animate__bounceInRight",
+//                     "animate__zoomOutRight",
+//                     "animate__bounceOutRight",
+//                     "animate__fadeInRightBig",
+//                     "animate__fadeOutRight",
+//                     "animate__flipOutX",
+//                     "animate__flipOutY" ];
+// var start = "animate__animated";
+
+
+
+
+
+
+// $("h1").hover(function () {
+
+//     var name = Math.floor((Math.random() * 10) + 1);
+//     $(this).addClass(start + " " + animations[2]);
+//     setTimeout(function () {
+//         $("h1").removeClass(start + " " + animations[2]);
+//     }, 1000);
+
+// });
+
+
+
+
+
+// $("li a").hover(function () {
+//     var start = "animate__animated";
+
+//     $(this).addClass(start + " " + animations[name]);
+//     setTimeout(function () {
+//         $("li a").removeClass(start + " " + animations[name]);
+//     }, 1500);
+// })
+
